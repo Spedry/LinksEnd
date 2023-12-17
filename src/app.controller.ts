cad5143fcda536ff1273779f02ee834f3c7d0232
Spedry <spedrysk@gmail.com>
2023-12-17 20:07:46 +0100

    provide angular app html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  /*    @Get()
+          getHello(): string {
+              return this.appService.getHello();
+          }*/
+
+  @Get('/**')
+  get(@Res() res: Response) {
+    this.logger.debug('Sending angular app');
+    res.sendFile('index.html', {
+      root: '../../Links/Links/dist/links/browser',
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Serve static files from the Angular app
+  app.use(express.static(path.join(__dirname, '../../Links/dist/links/browser')));
+
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(r => console.log("App started"));
